d3ee5ceca69e20bb1882c9c4000a4d42a15447fc
Lucio Afonso <lucioafonso@icloud.com>
2020-07-09 12:55:32 +0200

    My second commit

--- routes/query.js
+++ routes/query.js
@@ -1,5 +1,6 @@
-const router = require("express").Router(),
-  request = require("request");
+const router = require("express").Router();
+const request = require("request");
+const arrTmp = [];
 
 function redirectToHome() {
   return res.redirect("/");
@@ -10,7 +11,8 @@ function validateInput(query) {
     redirectToHome();
 
   for (const key in query) {
-    if (key !== "src" && key !== "dst") redirectToHome();
+    if (key !== "src" && key !== "dst")
+      redirectToHome();
   }
 
   if (
@@ -20,73 +22,65 @@ function validateInput(query) {
   )
     redirectToHome();
 
-  let arrSrc = query["src"].split(","),
-    srcLat = parseFloat(arrSrc[0]),
-    srcLon = parseFloat(arrSrc[1]);
+  let arrSrc = query["src"].split(",");
+  let srcLat = parseFloat(arrSrc[0]);
+  let srcLon = parseFloat(arrSrc[1]);
 
-  if (isNaN(srcLat) || isNaN(srcLon)) redirectToHome();
+  if (isNaN(srcLat) || isNaN(srcLon))
+    redirectToHome();
 
   //TODO: Implement the above check for all dst
 }
 
-function restfulAPI(urlString){
-  return new Promise(function(resolve, reject) {
-    request.get(
-    {
-      url:urlString,
-      json: true,
-      headers: { "User-Agent": "request" },
-    },
-    (err, response, responseAPI) => {
-      if (err) {
-        console.error("ERROR: ");
-        console.error(err);
-      } else if (response.statusCode !== 200) {
-        console.error("Status: ");
-        console.error(response.statusCode);
-      } else {
-        resolve(responseAPI)
-        // res.cookie("distance"+key, responseAPI["routes"][0]["distance"], cookieOptions)
-        // data is already parsed as JSON:
-        // console.log("Got data: \n");
-        // console.log(responseAPI)
+function getUnique(arr, comp) {
+  // store the comparison  values in array
+  const unique = arr
+    .map((e) => e[comp])
 
-        // duration = parseFloat(responseAPI["routes"][0]["duration"]);
-        // distance = parseFloat(responseAPI["routes"][0]["distance"]);
-        // console.log(responseAPI["routes"][0]["duration"])
-        // console.log(duration);
-      }
-    });
-  });
+    // store the indexes of the unique objects
+    .map((e, i, final) => final.indexOf(e) === i && i)
+
+    // eliminate the false indexes & return unique objects
+    .filter((e) => arr[e])
+    .map((e) => arr[e]);
+
+  return unique;
 }
 
 router.get("/", (req, res) => {
   validateInput(req.query);
-  let duration, distance;
-  const routes = [],
-    { src, dst } = req.query;
+
+  const { src, dst } = req.query,
+    routes = [];
 
   for (const key in dst) {
-    responseAPI = restfulAPI("http://router.project-osrm.org/route/v1/driving/" +
-    src +
-    ";" +
-    dst[key] +
-    "?overview=false")
-    // .then((responseAPI) => {
-    //   duration = parseFloat(responseAPI["routes"][0]["duration"]);
-    //   distance = parseFloat(responseAPI["routes"][0]["distance"]);
-    //   console.log(duration)
-    // })
-    console.log(responseAPI)
+    let urlString =
+      "http://router.project-osrm.org/route/v1/driving/" + src + ";" + dst[key] + "?overview=false";
 
-    // routes.push({
-    //   destination: dst[key],
-    //   duration: duration,
-    //   distance: distance,
-    // });
+    request.get({
+        url: urlString,
+        json: true,
+        headers: { "User-Agent": "request" },
+      },
+      (err, response, responseAPI) => {
+        if (err) {
+          console.error("ERROR: ");
+          console.error(err);
+        } else if (response.statusCode !== 200) {
+          console.error("Status: ");
+          console.error(response.statusCode);
+        } else {
+          arrTmp.push({
+            destination: dst[key],
+            duration: responseAPI["routes"][0]["duration"],
+            distance: responseAPI["routes"][0]["distance"],
+          });
+        }
+      }
+    );
   }
 
-  routes.sort((objA, objB) => {
+  arr.sort((objA, objB) => {
     if (objA["duration"] < objB["duration"]) {
       return -1;
     } else if (objA["duration"] > objB["duration"]) {
@@ -100,13 +94,8 @@ router.get("/", (req, res) => {
     return 0;
   });
 
-  //TODO: Check code OK
-  //TODO: sort from mobile array and objects : Developer.Mozilla "array sort"
-  //TODO: get the API from the bois and do multi bs
+  routes.push(getUnique(arrTmp, "destination"));
 
-  // console.log("src: "+ typeof src);
-
-  // console.log(req.query)
   res.status(200).json({
     source: src,
     routes: routes,
